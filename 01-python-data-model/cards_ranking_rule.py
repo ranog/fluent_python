a1c78dedac56e58362f45276f3b8fb9372e6fd9f
ranog <jprnogueira@yahoo.com.br>
2021-11-04 08:57:05 -0300

    implementation of card ranking rules

---
+++ 01-python-data-model/cards_ranking_rule.py
@@ -0,0 +1,13 @@
+from frenchdeck import FrenchDeck
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+deck = FrenchDeck()
+for card in sorted(deck, key=spades_high):
+    print(card)
