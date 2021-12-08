e63597c93943f7eacf1d67abb988e118f8d73921
ranog <jprnogueira@yahoo.com.br>
2021-12-08 09:03:54 -0300

    ex 2-4. Cartesian product using a list comprehension

---
+++ 02-an-array-of-sequences/t-shirt_cartesian_product.py
@@ -0,0 +1,16 @@
+colors = ['black', 'white']
+sizes = ['S', 'M', 'L']
+
+tshirts = [(color, size) for color in colors for size in sizes]
+
+print(tshirts)
+print()
+
+for color in colors:
+    for size in sizes:
+        print((color, size))
+
+tshirts = [(color, size) for size in sizes for color in colors]
+
+print()
+print(tshirts)
