1b426d2e5e09eb7916f9fb91f8c3adb44b8f2c91
ranog <jprnogueira@yahoo.com.br>
2021-12-13 08:29:53 -0300

    determine explicitly if a tuple (or any object) has a fixed value

---
+++ 02-an-array-of-sequences/fixed_function.py
@@ -0,0 +1,13 @@
+def fixed(o):
+    try:
+        hash(o)
+    except TypeError:
+        return False
+    return True
+
+
+tuple_fixed = (10, 'alpha', (1, 2))
+tuple_mutable = (10, 'alpha', [1, 2])
+
+print(fixed(tuple_fixed))
+print(fixed(tuple_mutable))
