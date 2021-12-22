8fca3ce7ede58a0f20ff909f25b0b290df4544bc
ranog <jprnogueira@yahoo.com.br>
2021-12-22 17:58:49 -0300

    using * to grab excess items

--- 02-an-array-of-sequences/filesystem_path.py
+++ 02-an-array-of-sequences/filesystem_path.py
@@ -2,3 +2,18 @@ import os
 
 _, filename = os.path.split('$HOME/.ssh/id_rsa.pub')
 print(filename)
+
+a, b, *rest = range(5)
+print(a, b, rest)
+
+a, b, *rest = range(3)
+print(a, b, rest)
+
+a, b, *rest = range(2)
+print(a, b, rest)
+
+a, *body, c, d = range(5)
+print(a, body, c, d)
+
+*head, b, c, d = range(5)
+print(head, b, c, d)
