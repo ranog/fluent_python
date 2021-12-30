779e61a6097002c4dc923b1205b114b94c8b04db
ranog <jprnogueira@yahoo.com.br>
2021-12-30 09:12:07 -0300

    ex 2-11: pattern matching with match/case - requires python >= 3.10

---
+++ 02-an-array-of-sequences/matching_patterns_with_match_and_case.py
@@ -0,0 +1,28 @@
+class Symbol:
+    pass
+
+
+class Procedure:
+    pass
+
+
+def evaluate(exp, env):
+    """Evaluate an expression in an environment."""
+    match exp:
+        # case ...:
+        #     several lines omitted
+        #     ...
+        case ['quote', exp]:
+            return exp
+        case ['if', test, consequence, alternative]:
+            if evaluate(test, env):
+                return evaluate(consequence, env)
+            else:
+                return evaluate(alternative, env)
+        case ['define', Symbol(name), exp]:
+            env[name] = evaluate(exp, env)
+        case ['lambda', parms, *body] if body:
+            return Procedure(parms, body, env)
+        # more lines omitted
+        case _:
+            raise SyntaxError(repr(exp))
--- 02-an-array-of-sequences/matching_patterns_without_match_and_case.py
+++ 02-an-array-of-sequences/matching_patterns_without_match_and_case.py
@@ -1,4 +1,4 @@
-class Procedure():
+class Procedure:
     pass
 
 
