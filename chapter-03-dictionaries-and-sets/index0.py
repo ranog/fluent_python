a16b10794e027fccfcc069f6f9762937bbcadb37
ranog <jprnogueira@yahoo.com.br>
2022-01-28 18:46:09 -0300

    uses dict.get to fetch and update a list of word occurrences from the index

---
+++ chapter-03-dictionaries-and-sets/index0.py
@@ -0,0 +1,22 @@
+"""Build an index mapping word -> list of occurrences"""
+
+import re
+import sys
+
+WORD_RE = re.compile(r'\w+')
+
+index = {}
+with open(sys.argv[1], encoding='utf-8') as fp:
+    for line_no, line in enumerate(fp, 1):
+        for match in WORD_RE.finditer(line):
+            word = match.group()
+            column_no = match.start() + 1
+            location = (line_no, column_no)
+            # this is ugly; coded like this to make a point
+            occurrences = index.get(word, [])
+            occurrences.append(location)
+            index[word] = occurrences
+
+# display in alphabetical order
+for word in sorted(index, key=str.upper()):
+    print(word, index[word])
