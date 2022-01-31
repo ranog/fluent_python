b3419a015bc4998e044f3481e14ddc020058ea35
ranog <jprnogueira@yahoo.com.br>
2022-01-31 08:52:41 -0300

    ex 3-8. StrKeyDict0 converts nonstring keys to str on lookup

---
+++ chapter-03-dictionaries-and-sets/str_key_dict_0.py
@@ -0,0 +1,35 @@
+class StrKeyDict0(dict):
+
+    def __missing__(self, key):
+        if isinstance(key, str):
+            raise KeyError(key)
+        return self[str(key)]
+
+    def get(self, key, default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def __contains__(self, key):
+        return key in self.keys() or str(key) in self.keys()
+
+
+d = StrKeyDict0([('2', 'two'), ('4', 'four')])
+
+print('Tests for item retrieval using "d.get(key)" notation::')
+print(d.get('2'))
+print(d.get(4))
+print(d.get(1, 'N/A'))
+print()
+
+print('Tests for the "in" operator::')
+print(2 in d)
+print(1 in d)
+print()
+
+print('Tests for item retrieval using "d[key]" notation::')
+print(d['2'])
+print(d[4])
+print(d[1])
+print()
