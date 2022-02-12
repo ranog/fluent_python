69a252ab282b7845350d130e37b2ef8e59b5838b
ranog <jprnogueira@yahoo.com.br>
2022-02-12 12:38:24 -0300

    add JSON-compatible aliases for Python’s True, False, and None so can paste JSON directly in the console

---
+++ chapter-03-dictionaries-and-sets/json_directly_in_the_console.py
@@ -0,0 +1,10 @@
+true, false, null = True, False, None
+fruit = {
+    "type": "banana",
+    "avg_weight": 123.2,
+    "edible_peel": false,
+    "species": ["acuminata", "balbisiana", "paradisiaca"],
+    "issues": null,
+}
+
+print(fruit)
