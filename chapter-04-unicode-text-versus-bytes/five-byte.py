cbb362c125d5bad3c49859e1ee859d62997f0d4c
ranog <jprnogueira@yahoo.com.br>
2022-02-15 21:46:49 -0300

    fromhex

--- chapter-04-unicode-text-versus-bytes/five-byte.py
+++ chapter-04-unicode-text-versus-bytes/five-byte.py
@@ -6,3 +6,6 @@ print(cafe[:1])
 cafe_arr = bytearray(cafe)
 print(cafe_arr)
 print(cafe_arr[-1:])
+
+# fromhex constrói uma sequência binária analisando pares de dígitos hexadecimais opcionalmente separados por espaços:
+print(bytes.fromhex('31 4b CE A9'))
