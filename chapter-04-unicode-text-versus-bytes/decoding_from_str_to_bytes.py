53c493b94cfe7894e94e56a4d87ab2f4c731df0e
ranog <jprnogueira@yahoo.com.br>
2022-02-17 23:31:54 -0300

    ex 4-6: decoding from str to bytes - success and error handling

---
+++ chapter-04-unicode-text-versus-bytes/decoding_from_str_to_bytes.py
@@ -0,0 +1,6 @@
+octets = b'Montr\xe9al'
+print(octets.decode('cp1252'))
+print(octets.decode('iso8859_7'))
+print(octets.decode('koi8_r'))
+print(octets.decode('utf_8', errors='replace'))
+print(octets.decode('utf_8'))
