e00657569cee5f1e8e802f73ca5c4cf304ce910b
ranog <jprnogueira@yahoo.com.br>
2022-02-19 17:16:35 -0300

    BOM: a useful Gremlin

---
+++ chapter-04-unicode-text-versus-bytes/extra_bytes_at_the_beginning.py
@@ -0,0 +1,11 @@
+u16 = 'El Niño'.encode('utf_16')
+u16le = 'El Niño'.encode('utf_16le')
+u16be = 'El Niño'.encode('utf_16be')
+
+print(u16)
+print(u16le)
+print(u16be)
+
+print(list(u16))
+print(list(u16le))
+print(list(u16be))
