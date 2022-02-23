f57b19da30474ee61a3201a124a1b15b145402a4
ranog <jprnogueira@yahoo.com.br>
2022-02-22 21:49:03 -0300

    ex 4-9: closer inspection of Example 4-8 running on Windows reveals the bug and how to fix it

--- chapter-04-unicode-text-versus-bytes/encoding_issue.py
+++ chapter-04-unicode-text-versus-bytes/encoding_issue.py
@@ -1,2 +1,21 @@
-open('cafe.txt', 'w', encoding='utf_8').write('café')
-open('cafe.txt').read()
+import os
+
+fp = open('cafe.txt', 'w', encoding='utf_8')
+print(fp)
+print(fp.write('café'))
+fp.close()
+
+print(os.stat('cafe.txt').st_size)
+
+fp2 = open('cafe.txt')
+print(fp2)
+print(fp2.encoding)
+print(fp2.read())
+
+fp3 = open('cafe.txt', encoding='utf_8')
+print(fp3)
+print(fp3.read())
+
+fp4 = open('cafe.txt', 'rb')
+print(fp4)
+print(fp4.read())
