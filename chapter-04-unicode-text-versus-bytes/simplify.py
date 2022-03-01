c76fbc4e4f3df355f5533dc347b4c0aed0fb0f93
ranog <jprnogueira@yahoo.com.br>
2022-03-01 20:14:36 -0300

    ex 4-15: two examples using shave_marks from ex 4-14

--- chapter-04-unicode-text-versus-bytes/simplify.py
+++ chapter-04-unicode-text-versus-bytes/simplify.py
@@ -7,3 +7,10 @@ def shave_marks(txt):
     norm_txt = unicodedata.normalize('NFD', txt)
     shaved = ''.join(c for c in norm_txt if not unicodedata.combining(c))
     return unicodedata.normalize('NFC', shaved)
+
+
+order = '“Herr Voß: • ½ cup of Œtker™ caffè latte • bowl of açaí.”'
+print(shave_marks(order))
+
+Greed = 'Ζέφυρος, Zéfiro'
+print(shave_marks(Greed))
