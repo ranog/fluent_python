f7bfda54365fcd18c985883401e4e66bdd14ea01
ranog <jprnogueira@yahoo.com.br>
2022-03-02 09:55:28 -0300

    ex 4-16: function to remove combining marks from Latin characters

--- chapter-04-unicode-text-versus-bytes/simplify.py
+++ chapter-04-unicode-text-versus-bytes/simplify.py
@@ -1,5 +1,5 @@
 import unicodedata
-# import string
+import string
 
 
 def shave_marks(txt):
@@ -9,8 +9,26 @@ def shave_marks(txt):
     return unicodedata.normalize('NFC', shaved)
 
 
+def shave_marks_latin(txt):
+    """Remove all diacritic marks from Latin base characters"""
+    norm_txt = unicodedata.normalize('NFD', txt)
+    latin_base = False
+    preserve = []
+    for c in norm_txt:
+        if unicodedata.combining(c) and latin_base:
+            continue    # ignore diacritic on Latin base char
+        preserve.append(c)
+        # if it isn't a combining char, it's a new base char
+        if not unicodedata.combining(c):
+            latin_base = c in string.ascii_letters
+    shaved = ''.join(preserve)
+    return unicodedata.normalize('NFC', shaved)
+
+
 order = '“Herr Voß: • ½ cup of Œtker™ caffè latte • bowl of açaí.”'
 print(shave_marks(order))
+print(shave_marks_latin(order))
 
 Greed = 'Ζέφυρος, Zéfiro'
 print(shave_marks(Greed))
+print(shave_marks_latin(Greed))
