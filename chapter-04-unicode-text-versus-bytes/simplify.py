9070cff1dda530cd6e79e1812f57170cf2fee9dc
ranog <jprnogueira@yahoo.com.br>
2022-03-03 13:32:25 -0300

    ex 4-18: two examples using asciize from example 4-17

--- chapter-04-unicode-text-versus-bytes/simplify.py
+++ chapter-04-unicode-text-versus-bytes/simplify.py
@@ -32,3 +32,37 @@ print(shave_marks_latin(order))
 Greed = 'Ζέφυρος, Zéfiro'
 print(shave_marks(Greed))
 print(shave_marks_latin(Greed))
+
+print()
+print('##############################################################')
+print()
+
+single_map = str.maketrans("""‚ƒ„ˆ‹‘’“”•–—˜›""", """'f"^<''""---~>""")
+multi_map = str.maketrans({
+    '€': 'EUR',
+    '…': '...',
+    'Æ': 'AE',
+    'æ': 'ae',
+    'Œ': 'OE',
+    'œ': 'oe',
+    '™': '(TM)',
+    '‰': '<per mille>',
+    '†': '**',
+    '‡': '***',
+})
+multi_map.update(single_map)
+
+
+def dewinize(txt):
+    """Replace Win1252 symbols with ASCII chars or sequences"""
+    return txt.translate(multi_map)
+
+
+def asciize(txt):
+    no_marks = shave_marks_latin(dewinize(txt))
+    no_marks = no_marks.replace('ß', 'ss')
+    return unicodedata.normalize('NFKC', no_marks)
+
+
+print(dewinize(order))
+print(asciize(order))
