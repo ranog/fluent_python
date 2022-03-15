b84b1d9f2c0dd73274bec4c96d13c87c485a9cfc
ranog <jprnogueira@yahoo.com.br>
2022-03-15 13:39:37 -0300

    ex 5-5: Named tuple attributes and methods

--- chapter-05-data-class-builders/namedtuple_information_about_a_city.py
+++ chapter-05-data-class-builders/namedtuple_information_about_a_city.py
@@ -1,9 +1,17 @@
+import json
 from collections import namedtuple
 
-City = namedtuple('City', 'name country population coordinates')
+City = namedtuple(typename='City', field_names='name country population coordinates')
 tokyo = City(name='Tokyo', country='JP', population=36.933, coordinates=(35.689722, 139.691667))
 
 print(tokyo)
 print(tokyo.population)
 print(tokyo.coordinates)
 print(tokyo[1])
+print(City._fields)
+
+Coordinate = namedtuple(typename='Coordinate', field_names='lat lon')
+delhi_data = ('Dalhi NCR', 'IN', 21.935, Coordinate(lat=28.613889, lon=77.208889))
+delhi = City._make(delhi_data)
+print(delhi._asdict())
+print(json.dumps(delhi._asdict()))
