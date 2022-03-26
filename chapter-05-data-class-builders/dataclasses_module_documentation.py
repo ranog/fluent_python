af06d08b5c36565a2391f736c1e5c3f3d09bc5ab
ranog <jprnogueira@yahoo.com.br>
2022-03-26 11:01:28 -0300

    ex 5-18: Example from the dataclasses module documentation

---
+++ chapter-05-data-class-builders/dataclasses_module_documentation.py
@@ -0,0 +1,15 @@
+from dataclasses import InitVar, dataclass
+
+
+@dataclass
+class C:
+    i: int
+    j: int = None
+    database: InitVar[DatabaseType] = None
+
+    def __post_init__(self, database):
+        if self.j is None and database is not None:
+            self.j = database.lookup('j')
+
+
+c = C(i=10, database=my_database)
