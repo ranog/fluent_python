1625750e58b92e93c22a1f2439f4d3dce5165940
ranog <jprnogueira@yahoo.com.br>
2022-04-01 20:09:57 -0300

    ex 5-22: City class and a few instances

---
+++ chapter-05-data-class-builders/keyword_class_patterns.py
@@ -0,0 +1,28 @@
+import typing
+
+
+class City(typing.NamedTuple):
+    continent: str
+    name: str
+    country: str
+
+
+cities = [
+    City(continent='Asia', name='Tokyo', country='JP'),
+    City(continent='Asia', name='Delhi', country='IN'),
+    City(continent='North America', name='Mexico City', country='MX'),
+    City(continent='North America', name='New York', country='US'),
+    City(continent='South America', name='São Paulo', country='BR'),
+]
+
+
+def match_asian_cities():
+    results = []
+    for city in cities:
+        match city:
+            case City(continent='Asia'):
+                results.append(city)
+    return results
+
+
+print(match_asian_cities())
