03969f01f9296f1744d7e2768d02ca81f707466f
ranog <jprnogueira@yahoo.com.br>
2022-04-11 08:54:55 -0300

    ex 6-4: alex and charles compare equal, but alex is not charles

--- chapter-06-object-references-mutability-and-recycling/charles_and_lewis.py
+++ chapter-06-object-references-mutability-and-recycling/charles_and_lewis.py
@@ -6,3 +6,7 @@ print(id(charles), id(lewis))
 
 lewis['balance'] = 950
 print(charles)
+
+alex = {'name': 'Charles L. Dodgson', 'born': 1832, 'balance': 950}
+print(alex == charles)
+print(alex is not charles)
