a7c618ba3d2ea6ae2b854ffaf045fc91f8ca7b42
ranog <jprnogueira@yahoo.com.br>
2022-04-16 14:09:28 -0300

    ex 6-5: t1 and t2 initially compare equal, but changing a mutable item inside tuple t1 makes it different

---
+++ chapter-06-object-references-mutability-and-recycling/changing_a_mutable_item_inside_tuple.py
@@ -0,0 +1,12 @@
+t1 = (1, 2, [30, 40])
+t2 = (1, 2, [30, 40])
+
+print(t1 == t2)
+print(f't1 = {t1}')
+print(id(t1[-1]))
+
+t1[-1].append(99)
+print(f't1 = {t1}')
+print(id(t1[-1]))
+
+print(t1 == t2)
