a36cd525cd181dc3899fae5d05053efd4d8cff8a
ranog <jprnogueira@yahoo.com.br>
2022-05-03 19:49:06 -0300

    ex 7-5: list of factorials produced with map and filter compared to alternatives coded as list comprehensions

--- chapter-07-functions-as-first-class-objects/factorial.py
+++ chapter-07-functions-as-first-class-objects/factorial.py
@@ -6,4 +6,10 @@ def factorial(n):
 print(factorial(42))
 print(factorial.__doc__)
 print(type(factorial))
-print(help(factorial))
+# print(help(factorial))
+
+print(list(map(factorial, range(6))))
+print([factorial(n) for n in range(6)])
+
+print(list(map(factorial, filter(lambda n: n % 2, range(6)))))
+print([factorial(n) for n in range(6) if n % 2])
