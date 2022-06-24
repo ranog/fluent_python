d37a947790523ec6673ee6579daa58004005e52c
ranog <jprnogueira@yahoo.com.br>
2022-06-24 08:58:05 -0300

    ex 8-13: returns a list of tuples of strings

---
+++ chapter-08-type-hints-in-functions/columnize.py
@@ -0,0 +1,18 @@
+from collections.abc import Sequence
+
+
+def columnize(sequence: Sequence[str], num_columns: int = 0) -> list[tuple[str, ...]]:
+    if not num_columns:
+        num_columns = round(len(sequence) ** 0.5)
+    num_rows, reminder = divmod(len(sequence), num_columns)
+    num_rows += bool(reminder)
+    return [tuple(sequence[i::num_rows]) for i in range(num_rows)]
+
+
+animals = 'drake fawn heron ibex koala lynx tahr xerus yak zapus'.split()
+table = columnize(animals)
+
+print(table)
+
+for row in table:
+    print(''.join(f'{word:10}' for word in row))
