8591cec4f5db1a016f7d0ca0905e8fe3efb4cd13
ranog <jprnogueira@yahoo.com.br>
2022-07-02 13:35:35 -0300

    ex 8-21: definition of the top function using a TypeVar with bound=SupportsLessThan

--- chapter-08-type-hints-in-functions/top.py
+++ chapter-08-type-hints-in-functions/top.py
@@ -1,10 +1,13 @@
 from collections.abc import Iterable
 from typing import TypeVar
 
-T = TypeVar('T')
+from comparable import SupportsLessThan
 
 
-def top(series: Iterable[T], length: int) -> list[T]:
+LT = TypeVar('LT', bound=SupportsLessThan)
+
+
+def top(series: Iterable[LT], length: int) -> list[LT]:
     ordered = sorted(series, reverse=True)
     return ordered[:length]
 
