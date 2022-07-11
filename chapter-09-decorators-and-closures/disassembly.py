863bac4c1c6772463fdee76d3eac8446a699f986
ranog <jprnogueira@yahoo.com.br>
2022-07-11 08:55:11 -0300

    ex 9-5 and 9-6: disassembly

---
+++ chapter-09-decorators-and-closures/disassembly.py
@@ -0,0 +1,12 @@
+from dis import dis
+
+from reading_a_local_and_a_global_variable import f1, f2
+
+
+dis(f1)
+
+print()
+print('___________________//___________________')
+print()
+
+dis(f2)
--- chapter-09-decorators-and-closures/reading_a_local_and_a_global_variable.py
+++ chapter-09-decorators-and-closures/reading_a_local_and_a_global_variable.py
@@ -4,6 +4,7 @@ def f1(a):
 
 
 b = 6
+f1(3)
 
 
 def f2(a):
@@ -12,4 +13,18 @@ def f2(a):
     b = 9
 
 
-f2(3)
+# f2(3)
+
+
+# b = 6
+
+
+# def f3(a):
+#     global b
+#     print(a)
+#     print(b)
+#     b = 9
+
+
+# f3(3)
+# print(b)
